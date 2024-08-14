bd79713a53728151d841178fbd3c0f1fdda10288
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-08-14 13:56:33 +0300

    fix some styles and logic

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
+function toggleBodyScroll(shouldDisable) {
+  if (shouldDisable) {
+    document.body.classList.add('no-scroll');
+  } else {
+    document.body.classList.remove('no-scroll');
+  }
+}
+
 document.querySelector('.burger-btn').addEventListener('click', function () {
   document.querySelector('.mobile-menu').classList.add('active');
+  toggleBodyScroll(true);
 });
 
 document.querySelector('.close-btn').addEventListener('click', function () {
   document.querySelector('.mobile-menu').classList.remove('active');
+  toggleBodyScroll(false);
 });
 
 document.querySelector('.mobile-nav-btn').addEventListener('click', function () {
   document.querySelector('.mobile-menu').classList.remove('active');
+  toggleBodyScroll(false);
   openModal();
 });
 
@@ -18,9 +29,15 @@ mobileNavLinks.forEach(link => {
   });
 });
 
+document.querySelector('.mobile-sign-btn').addEventListener('click', function () {
+  document.querySelector('.mobile-menu').classList.remove('active');
+});
+
 /*modal window*/
+
 const modal = document.getElementById('exampleModalCenter');
-const openModalButtons = document.querySelectorAll('[data-toggle="modal"], .hero-button, mobile-nav-btn');
+const openModalButtons = document.querySelectorAll('[data-toggle="modal"], .hero-button, .mobile-nav-btn, .slider-info__btn');
+
 const closeModalButton = document.querySelector('.modal-close');
 
 const openModal = () => {
